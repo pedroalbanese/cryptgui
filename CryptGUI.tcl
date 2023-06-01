84e90cf0b1c97bda9787f4cce5ca7a57f1885645
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-06-01 15:25:35 -0300

    CryptGUI written in TCL/TK

--- CryptGUI.tcl
+++ CryptGUI.tcl
@@ -39,58 +39,58 @@ grid .decryptButton -row 2 -column 4 -sticky e -padx 10 -pady 5
 
 # Function to perform encryption
 proc encrypt {} {
-    global keyBox ivBox
-    set plaintext [.plaintextBox get 1.0 end]
-    set key [.keyBox get]
-    set iv [.ivBox get]
+	global keyBox ivBox
+	set plaintext [.plaintextBox get 1.0 end]
+	set key [.keyBox get]
+	set iv [.ivBox get]
 
-    # Perform encryption logic here
-    set pad [expr {16 - ([string length $plaintext] % 16)}]
-    append plaintext [string repeat [format %c $pad] $pad]
-    set encryptedMsg [::aes::aes -dir encrypt -iv $iv -key $key $plaintext]
+	# Perform encryption logic here
+	set pad [expr {16 - ([string length $plaintext] % 16)}]
+	append plaintext [string repeat [format %c $pad] $pad]
+	set encryptedMsg [::aes::aes -dir encrypt -iv $iv -key $key $plaintext]
 
-    # Update ciphertext box with the encrypted result
-    .ciphertextBox delete 1.0 end
-    .ciphertextBox insert 1.0 [binary encode base64 $encryptedMsg]
+	# Update ciphertext box with the encrypted result
+	.ciphertextBox delete 1.0 end
+	.ciphertextBox insert 1.0 [binary encode base64 $encryptedMsg]
 }
 
 proc removePadding {text} {
-    # Find last byte of padding
-    set lastByte [expr {[scan [string range $text end-1 end] %c]}]
+	# Find last byte of padding
+	set lastByte [expr {[scan [string range $text end-1 end] %c]}]
 
-    # Verify if the padding is valid
-    set isValidPadding 1
-    for {set i [expr {[string length $text] - $lastByte}]} {$i < [string length $text]} {incr i} {
-        if {[scan [string index $text $i] %c] != $lastByte} {
-            set isValidPadding 0
-            break
-        }
-    }
+	# Verify if the padding is valid
+	set isValidPadding 1
+	for {set i [expr {[string length $text] - $lastByte}]} {$i < [string length $text]} {incr i} {
+		if {[scan [string index $text $i] %c] != $lastByte} {
+			set isValidPadding 0
+			break
+		}
+	}
 
-    if {$isValidPadding} {
-        # Remove the padding
-        set text [string range $text 0 [expr {[string length $text] - $lastByte - 1}]]
-    }
+	if {$isValidPadding} {
+		# Remove the padding
+		set text [string range $text 0 [expr {[string length $text] - $lastByte - 1}]]
+	}
 
-    return $text
+	return $text
 }
 
 
 # Function to perform decryption
 proc decrypt {} {
-    global keyBox ivBox
-    set ciphertext [.ciphertextBox get 1.0 end]
-    set key [.keyBox get]
-    set iv [.ivBox get]
+	global keyBox ivBox
+	set ciphertext [.ciphertextBox get 1.0 end]
+	set key [.keyBox get]
+	set iv [.ivBox get]
 
-    # Perform decryption logic here
-    set decryptedMsg [::aes::aes -dir decrypt -iv $iv -key $key [binary decode base64 $ciphertext]]
-    set decryptedMsg [removePadding $decryptedMsg]
-    set decryptedMsg [string trim $decryptedMsg]
+	# Perform decryption logic here
+	set decryptedMsg [::aes::aes -dir decrypt -iv $iv -key $key [binary decode base64 $ciphertext]]
+	set decryptedMsg [removePadding $decryptedMsg]
+	set decryptedMsg [string trim $decryptedMsg]
 
-    # Update plaintext box with the decrypted result
-    .plaintextBox delete 1.0 end
-    .plaintextBox insert 1.0 $decryptedMsg
+	# Update plaintext box with the decrypted result
+	.plaintextBox delete 1.0 end
+	.plaintextBox insert 1.0 $decryptedMsg
 }
 
 # Start the event loop
